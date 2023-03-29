fa0b1ca00a140ca9e5d04f4f0ad94f628d604ab0
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-03-28 19:34:47 -0500

    add each cocktail in individual div and added eventlistener for keyup (#2)

--- main.js
+++ main.js
@@ -1,74 +1,114 @@
 const drinkSection = document.getElementById("drink-wrap");
-const drinkIngredient = document.getElementsByClassName('drinkIngredient')
-const fetchUrl = 'https://the-cocktail-db.p.rapidapi.com/randomselection.php';
+const drinkIngredient = document.getElementsByClassName("drinkIngredient");
+const fetchUrl = "https://the-cocktail-db.p.rapidapi.com/randomselection.php";
+const searchInput = document.getElementById("search-input");
+const searchOptions = document.getElementById("search-options");
+const COCKTAIL_KEY = "da12acd15cmsh2552fb046e26223p151904jsnef737cab5849";
+const COCKTAIL_URL = "the-cocktail-db.p.rapidapi.com";
+const RANDOM_SELECTION_URL =
+  "https://the-cocktail-db.p.rapidapi.com/randomselection.php";
 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'da12acd15cmsh2552fb046e26223p151904jsnef737cab5849',
-		'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com'
-	}
+const cocktailSettings = {
+  url: RANDOM_SELECTION_URL,
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": COCKTAIL_KEY,
+    "X-RapidAPI-Host": COCKTAIL_URL,
+  },
 };
-function getCocktails() {
-fetch(fetchUrl, options)
-	.then(response => response.json())
-	.then((data) => {
-		// console.log(data)
-		// console.log(data.drinks[0])
-		// console.log(data.drinks[0].strDrink)
-		displayCocktails(data)
-	})
-	.catch(err => console.error(err));
+
+
+const fetchData = async () => {
+  try {
+    const response = await fetch(cocktailSettings.url, cocktailSettings);
+    const data = await response.json();
+    displayCocktails(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+async function displayCocktails(cocktail) {
+  const cocktails = cocktail.drinks;
+
+  cocktails.forEach((item) => {
+    const section = document.createElement("section");
+
+    const div = document.createElement("div");
+    div.className = "drink";
+    const drinkName = item.strDrink;
+    div.append(drinkName);
+    section.append(div);
+
+    const img = document.createElement("img");
+    img.src = item.strDrinkThumb;
+    div.append(img);
+    drinkSection.append(div);
+
+    const div2 = document.createElement("div");
+    div2.className = "drinkIngredient";
+    const divIngridient = document.createElement("div");
+    const divMeasure = document.createElement("div");
+    divMeasure.className = "measure-container";
+    divIngridient.className = "ingridients-container";
+
+    for (const key in item) {
+      if (key.startsWith("strMeasure") && item[key]) {
+        const measure = document.createElement("div");
+        measure.textContent = item[key];
+        divMeasure.append(measure);
+        div2.insertBefore(divMeasure, div2.children[0]);
+      }
+      if (key.startsWith("strIngredient") && item[key]) {
+        const ingredient = document.createElement("div");
+        ingredient.textContent = item[key];
+        divIngridient.append(ingredient);
+        div2.append(divIngridient);
+      }
+    }
+    div.append(div2);
+    const div3 = document.createElement("div");
+    div3.className = "drinkInstructions";
+    const instructions = item.strInstructions;
+    div3.append(instructions);
+    drinkSection.append(div3);
+  });
 }
 
-getCocktails();
+// INPUT SECTION
 
-// drinkSection.addEventListener("click", function(){
-// 	if(drinkIngredient.style.display === "block") {
-// 		drinkIngredient.style.display = "none";
-// 	} else {
-// 		drinkIngredient.style.display = 'block';
-// 	}
-// });
+const handleKeyUp = async (e) => {
+  const query = e.target.value.trim();
 
-function displayCocktails(cocktail) {
-	console.log(cocktail)
+  if (!query) {
+    searchOptions.innerHTML = "";
+    return;
+  }
+  try {
+    const response = await fetch(
+      `https://the-cocktail-db.p.rapidapi.com/search.php?s=${query}`,
+      cocktailSettings
+    );
+    const data = await response.json();
+    await doOptionsForInput(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-	for(let i=0; i < 10; i++) {
-		const div = document.createElement('div');
-		div.className = "drink";
-		const drinkName = cocktail.drinks[i].strDrink;
-		div.append(drinkName);
-		drinkSection.append(div);
-		
-		let img = document.createElement('img');
-		img.src = cocktail.drinks[i].strDrinkThumb;
-		div.append(img);
-		drinkSection.append(div);
-
-		for(let j=1; j<16; j++){
-
-			if (cocktail.drinks[i][`strIngredient${j}`] == null) {
-				break;
-			} 
-			if (cocktail.drinks[i][`strMeasure${j}`] == null) {
-				break;
-			}
-			else {
-				const div2 = document.createElement('div')
-				div2.className = "drinkIngredient";
-				const ingredient = cocktail.drinks[i][`strMeasure${j}`] + cocktail.drinks[i][`strIngredient${j}`];
-				div2.append(ingredient);
-				drinkSection.append(div2);
-			}
-		}
-
-		const div3 = document.createElement('div');
-		div3.className = "drinkInstructions"
-		const instructions = cocktail.drinks[i].strInstructions;
-		div3.append(instructions);
-		drinkSection.append(div3);
-	}
+async function doOptionsForInput(data) {
+  console.log(data);
+  searchOptions.textContent = "";
+  await data.drinks.forEach((result) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = result.strDrink;
+    searchOptions.appendChild(optionElement);
+  });
 }
 
-
+fetchData();
+searchOptions.addEventListener("input", (e) => {
+  searchInput.value = e.target.value;
+});
+searchInput.addEventListener("keyup", handleKeyUp);
