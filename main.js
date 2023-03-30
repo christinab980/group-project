dd890f22da87dfc2b763c3cebcfbb95055b52cb3
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-03-30 15:07:05 -0500

    Make each image in div (#4)
    * add each cocktail in individual div and added eventlistener for keyup
    
    * add changes to css
    
    * add onclick toggle drink

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ const COCKTAIL_KEY = "da12acd15cmsh2552fb046e26223p151904jsnef737cab5849";
 const COCKTAIL_URL = "the-cocktail-db.p.rapidapi.com";
 const RANDOM_SELECTION_URL =
   "https://the-cocktail-db.p.rapidapi.com/randomselection.php";
+const allTitles = document.querySelectorAll("[drink-id]");
+
+const cocktailsStage = [];
 
 const cocktailSettings = {
   url: RANDOM_SELECTION_URL,
@@ -17,65 +20,129 @@ const cocktailSettings = {
   },
 };
 
-
 const fetchData = async () => {
   try {
     const response = await fetch(cocktailSettings.url, cocktailSettings);
     const data = await response.json();
-    displayCocktails(data);
+    setCocktailStage(data.drinks);
   } catch (error) {
     console.log(error);
   }
 };
 
-
-async function displayCocktails(cocktail) {
-  const cocktails = cocktail.drinks;
-
-  cocktails.forEach((item) => {
-    const section = document.createElement("section");
-
+const displayCocktailsName = (cocktail) => {
+  cocktail.forEach((item) => {
     const div = document.createElement("div");
     div.className = "drink";
-    const drinkName = item.strDrink;
-    div.append(drinkName);
-    section.append(div);
-
-    const img = document.createElement("img");
-    img.src = item.strDrinkThumb;
-    div.append(img);
+    div.textContent = item.strDrink;
+    div.setAttribute("drink-id", item.idDrink);
     drinkSection.append(div);
-
-    const div2 = document.createElement("div");
-    div2.className = "drinkIngredient";
-    const divIngridient = document.createElement("div");
-    const divMeasure = document.createElement("div");
-    divMeasure.className = "measure-container";
-    divIngridient.className = "ingridients-container";
-
-    for (const key in item) {
-      if (key.startsWith("strMeasure") && item[key]) {
-        const measure = document.createElement("div");
-        measure.textContent = item[key];
-        divMeasure.append(measure);
-        div2.insertBefore(divMeasure, div2.children[0]);
-      }
-      if (key.startsWith("strIngredient") && item[key]) {
-        const ingredient = document.createElement("div");
-        ingredient.textContent = item[key];
-        divIngridient.append(ingredient);
-        div2.append(divIngridient);
-      }
-    }
-    div.append(div2);
-    const div3 = document.createElement("div");
-    div3.className = "drinkInstructions";
-    const instructions = item.strInstructions;
-    div3.append(instructions);
-    drinkSection.append(div3);
   });
+};
+
+function displayCocktails(cocktail, parentTag) {
+  console.log(parentTag.getAttribute("drink-id"));
+  const section = parentTag;
+
+  const div = document.createElement("div");
+  div.className = "drink";
+  div.id = "toggle-drink";
+
+  const img = document.createElement("img");
+  img.src = cocktail.strDrinkThumb;
+  div.append(img);
+
+  const div2 = document.createElement("div");
+  div2.className = "drinkIngredient";
+  const divIngridient = document.createElement("div");
+  const divMeasure = document.createElement("div");
+  divMeasure.className = "measure-container";
+  divIngridient.className = "ingridients-container";
+
+  cocktail.ingredients.forEach((item) => {
+    const measure = document.createElement("div");
+    measure.textContent = item;
+    divMeasure.append(measure);
+    div2.append(divMeasure);
+  });
+
+  cocktail.measures.forEach((item) => {
+    const ingredient = document.createElement("div");
+    ingredient.textContent = item;
+    divIngridient.append(ingredient);
+    div2.append(divIngridient);
+  });
+
+  div.append(div2);
+  const div3 = document.createElement("div");
+  div3.className = "drinkInstructions";
+  const instructions = cocktail.strInstructions;
+  div3.append(instructions);
+  div.append(div3);
+  section.append(div);
 }
 
+function handleCocktailClick(e) {
+  const toggleDrink = document.querySelector("#toggle-drink");
+
+  if (e.target.matches("[drink-id]")) {
+    if (toggleDrink) {
+      toggleDrink.remove();
+    }
+    const attribute = e.target.getAttribute("drink-id");
+    // Wipe out the left hand side dom
+    // Get the correct cocktail from cocktailsStage - Done
+    const targetedCocktail = getClickedCocktaild(attribute);
+    const targetedCocktail_tag = e.target;
+    displayCocktails(targetedCocktail, targetedCocktail_tag);
+    console.log(targetedCocktail);
+    // Render cocktail on the DOM
+  }
+}
+
+const getClickedCocktaild = (id) => {
+  if (!id) return;
+  if (id) {
+    const cleanKeyCocktail = getCleanDataFromSingleId(id, cocktailsStage);
+    return cleanKeyCocktail;
+  }
+};
+
+const getCleanDataFromSingleId = (id, stage) => {
+  const cocktail = stage.find((item) => item.idDrink === id);
+  if (!cocktail) return;
+  if (cocktail) {
+    const {
+      idDrink,
+      strAlcoholic,
+      strCategory,
+      strDrink,
+      strDrinkThumb,
+      strInstructions,
+      ingredients = [],
+      measures = [],
+    } = cocktail;
+    console.log(cocktail);
+    for (const key in cocktail) {
+      if (key.startsWith("strIngredient") && cocktail[key]) {
+        ingredients.push(cocktail[key]);
+      }
+      if (key.startsWith("strMeasure") && cocktail[key]) {
+        measures.push(cocktail[key]);
+      }
+    }
+    return (cleanCocktail = {
+      idDrink,
+      strAlcoholic,
+      strCategory,
+      strDrink,
+      strDrinkThumb,
+      strInstructions,
+      ingredients,
+      measures,
+    });
+  }
+};
 // INPUT SECTION
 
 const handleKeyUp = async (e) => {
@@ -97,6 +164,8 @@ const handleKeyUp = async (e) => {
   }
 };
 
+// GLOBAL FUNCTIONS
+
 async function doOptionsForInput(data) {
   console.log(data);
   searchOptions.textContent = "";
@@ -107,8 +176,20 @@ async function doOptionsForInput(data) {
   });
 }
 
-fetchData();
+function setCocktailStage(data) {
+  data.forEach((item) => {
+    cocktailsStage.push(item);
+  });
+}
+
+const getPageData = async () => {
+  await fetchData();
+  //  displayCocktails(cocktailsStage);
+  displayCocktailsName(cocktailsStage);
+};
+getPageData();
 searchOptions.addEventListener("input", (e) => {
   searchInput.value = e.target.value;
 });
 searchInput.addEventListener("keyup", handleKeyUp);
+document.addEventListener("click", handleCocktailClick);
